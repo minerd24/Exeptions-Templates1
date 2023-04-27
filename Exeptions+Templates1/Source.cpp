4e6c0be5ae5d5a8efb0a3a9ce004bda29c277e68
Evan Wall <minerd24@gmail.com>
2023-04-27 13:43:12 -0400

    Almost finished

--- Exeptions+Templates1/Source.cpp
+++ Exeptions+Templates1/Source.cpp
@@ -2,8 +2,41 @@
 // CIS 1202
 // 4/27/2023
 
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+
+using namespace std;
 
 char character(char start, int offset)
 {
+	int invalidCharacterException = NULL, invalidRangeException = NULL;
+	char target;
+	if (isalpha(start))
+	{
+		target = start + offset;
+	}
+	else
+		throw invalidCharacterException;
+	if (isalpha(target))
+	{
+		return target;
+	}
+	else
+		throw invalidRangeException;
+}
 
+int main()
+{
+	char driver = 'd', holdr;
+	int offDriver = '2';
+	try
+	{
+		holdr = character(driver, offDriver);
+		cout << holdr;
+	}
+	catch (int invalidCharacterException)
+	{
+		cout << "Whoops, That isn't correct."
+	}
 }
